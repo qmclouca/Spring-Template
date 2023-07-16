e00ffc448669882c77ed5a60b45dfe54882613f0
qmclouca <qmclouca@gmail.com>
2023-07-16 11:16:53 -0300

    improving client methods

--- src/main/java/com/qmclouca/base/services/ClientService.java
+++ src/main/java/com/qmclouca/base/services/ClientService.java
@@ -5,7 +5,7 @@ import com.qmclouca.base.models.Client;
 import java.util.List;
 
 public interface ClientService {
-    public abstract Client createClient(Client client);
-    public abstract List<Client> getClientsByName(String name);
-    public abstract List<Client> getAllClients();
+    Client createClient(Client client);
+    List<Client> getClientsByName(String name);
+    List<Client> getAllClients();
 }
