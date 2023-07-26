3cb38cb41a8cf6811985a87ea05a5dd3567f6413
qmclouca <qmclouca@gmail.com>
2023-07-25 21:01:54 -0300

    criando testes unitários

--- src/test/java/com/qmclouca/base/ClientControllerTests.java
+++ src/test/java/com/qmclouca/base/ClientControllerTests.java
@@ -22,6 +22,7 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import java.time.LocalDate;
@@ -169,18 +170,14 @@ public class ClientControllerTests{
         Mockito.when(clientService.getAllClients()).thenReturn(lstClients);
 
         // Perform the request to the endpoint
-        mockMvc.perform(MockMvcRequestBuilders.get("/api/clients"))
+        MvcResult result = mockMvc.perform(MockMvcRequestBuilders.get("/api/clients"))
                 .andExpect(MockMvcResultMatchers.status().isOk())
-                .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON))
-                .andExpect(MockMvcResultMatchers.jsonPath("$[0].name").value("John Doe"))
-                .andExpect(MockMvcResultMatchers.jsonPath("$[0].birthDate").value("1990-01-15"))
-                .andExpect(MockMvcResultMatchers.jsonPath("$[0].mobile").value("1234567890"))
-                .andExpect(MockMvcResultMatchers.jsonPath("$[0].email").value("john.doe@example.com"));
-
-        // Add more assertions based on the expected behavior and response data
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].name").value("Abelardo Barbosa"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[1].name").value("John Doe"))
+                .andReturn();
+        System.out.println("Response Content: " + result.getResponse().getContentAsString());
     }
 
-    // Helper method to convert objects to JSON strings
     private static String asJsonString(Object obj) {
         try {
             ObjectMapper mapper = new ObjectMapper();
